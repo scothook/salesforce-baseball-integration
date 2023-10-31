9afe6f60f7a2cc56aeb04e9069fb4e0a2d243c62
scothook <scot.hook@gmail.com>
2023-10-31 10:48:36 -0700

    Improve line score LWC draft

--- force-app/main/default/lwc/lineScore/lineScore.js
+++ force-app/main/default/lwc/lineScore/lineScore.js
@@ -1,3 +1,44 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class LineScore extends LightningElement {}
+export default class LineScore extends LightningElement {
+
+    @api homeTeam = 'Home Team'; // Set default home team name
+    @api awayTeam = 'Away Team'; // Set default away team name
+    @api innings = []; // Provide inning data in the format: [{ number: 1, home: 2, away: 1 }, ...]
+
+    get homeScores() {
+        return this.innings.map(inning => { return { number: inning.number, score: inning.home }; });
+    }
+
+    get awayScores() {
+        return this.innings.map(inning => { return { number: inning.number, score: inning.away }; });
+    }
+
+    get homeTotalRuns() {
+        return this.innings.reduce((total, inning) => total + inning.home, 0);
+    }
+
+    get awayTotalRuns() {
+        return this.innings.reduce((total, inning) => total + inning.away, 0);
+    }
+
+    get homeTotalHits() {
+        // Implement logic to calculate total hits for home team (if needed)
+        return 0;
+    }
+
+    get awayTotalHits() {
+        // Implement logic to calculate total hits for away team (if needed)
+        return 0;
+    }
+
+    get homeTotalErrors() {
+        // Implement logic to calculate total errors for home team (if needed)
+        return 0;
+    }
+
+    get awayTotalErrors() {
+        // Implement logic to calculate total errors for away team (if needed)
+        return 0;
+    }
+}
