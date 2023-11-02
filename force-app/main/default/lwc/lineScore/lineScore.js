a6e90f3b661ff255ccc50214442aca4a9807413c
scothook <scot.hook@gmail.com>
2023-11-02 11:48:55 -0700

    Added inning scores to Line Score LWC

--- force-app/main/default/lwc/lineScore/lineScore.js
+++ force-app/main/default/lwc/lineScore/lineScore.js
@@ -1,44 +1,70 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import { getRecord, getFieldValue } from "lightning/uiRecordApi";
+import { getRelatedListRecords } from 'lightning/uiRelatedListApi';
+
+import HOME_TOTAL_RUNS_FIELD from "@salesforce/schema/Game__c.Home_Team_Runs__c"
+import AWAY_TOTAL_RUNS_FIELD from "@salesforce/schema/Game__c.Away_Team_Runs__c"
+import HOME_TOTAL_HITS_FIELD from "@salesforce/schema/Game__c.Home_Team_Hits__c"
+import AWAY_TOTAL_HITS_FIELD from "@salesforce/schema/Game__c.Away_Team_Hits__c"
+import HOME_TOTAL_ERRORS_FIELD from "@salesforce/schema/Game__c.Home_Team_Errors__c"
+import AWAY_TOTAL_ERRORS_FIELD from "@salesforce/schema/Game__c.Away_Team_Errors__c"
+import HOME_TEAM_FIELD from "@salesforce/schema/Game__c.Home_Team__c"
+import AWAY_TEAM_FIELD from "@salesforce/schema/Game__c.Away_Team__c"
+
+const fields = [HOME_TOTAL_RUNS_FIELD, AWAY_TOTAL_RUNS_FIELD, HOME_TOTAL_HITS_FIELD, AWAY_TOTAL_HITS_FIELD, HOME_TOTAL_ERRORS_FIELD, AWAY_TOTAL_ERRORS_FIELD, HOME_TEAM_FIELD, AWAY_TEAM_FIELD];
 
 export default class LineScore extends LightningElement {
 
-    @api homeTeam = 'Home Team'; // Set default home team name
-    @api awayTeam = 'Away Team'; // Set default away team name
-    @api innings = []; // Provide inning data in the format: [{ number: 1, home: 2, away: 1 }, ...]
+    @api recordId;
+    error;
+    records;
 
-    get homeScores() {
-        return this.innings.map(inning => { return { number: inning.number, score: inning.home }; });
+    @wire(getRecord, { recordId: "$recordId", fields}) Game__c;
+
+    @wire(getRelatedListRecords, {
+        parentRecordId: '$recordId',
+        relatedListId: 'Innings__r',
+        fields: ['Inning__c.Inning_Number__c', 'Inning__c.Away_Team_Runs__c', 'Inning__c.Home_Team_Runs__c']
+    })
+    listInfo({ error, data }) {
+        if (data) {
+          this.records = data.records;
+          this.error = undefined;
+        } else if (error) {
+          this.error = error;
+          this.records = undefined;
+        }
     }
 
-    get awayScores() {
-        return this.innings.map(inning => { return { number: inning.number, score: inning.away }; });
+    get homeTeam() {
+        return getFieldValue(this.Game__c.data, HOME_TEAM_FIELD);
+    }
+
+    get awayTeam() {
+        return getFieldValue(this.Game__c.data, AWAY_TEAM_FIELD);
     }
 
     get homeTotalRuns() {
-        return this.innings.reduce((total, inning) => total + inning.home, 0);
+        return getFieldValue(this.Game__c.data, HOME_TOTAL_RUNS_FIELD);
     }
 
     get awayTotalRuns() {
-        return this.innings.reduce((total, inning) => total + inning.away, 0);
+        return getFieldValue(this.Game__c.data, AWAY_TOTAL_RUNS_FIELD);
     }
 
     get homeTotalHits() {
-        // Implement logic to calculate total hits for home team (if needed)
-        return 0;
+        return getFieldValue(this.Game__c.data, HOME_TOTAL_HITS_FIELD);
     }
 
     get awayTotalHits() {
-        // Implement logic to calculate total hits for away team (if needed)
-        return 0;
+        return getFieldValue(this.Game__c.data, AWAY_TOTAL_HITS_FIELD);
     }
 
     get homeTotalErrors() {
-        // Implement logic to calculate total errors for home team (if needed)
-        return 0;
+        return getFieldValue(this.Game__c.data, HOME_TOTAL_ERRORS_FIELD);
     }
 
     get awayTotalErrors() {
-        // Implement logic to calculate total errors for away team (if needed)
-        return 0;
+        return getFieldValue(this.Game__c.data, AWAY_TOTAL_ERRORS_FIELD);
     }
 }
